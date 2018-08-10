d509a0e9bb8508837a649ebaec7273345b483b41
C.S Wolf <37349534+CSneed7@users.noreply.github.com>
2018-08-09 21:28:14 -0400

    changed port

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
+// Sets up the Express App 
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 3360;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
